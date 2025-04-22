37691bfb845b4c89a414a08c01c9109071622567
ShankarSaradha <shankarsaradha3107@gmail.com>
2025-04-22 23:13:20 +0530

    Srjijith tension pandraan

---
+++ java/Swiper.js
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script>
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: { delay: 5000 },
+    });
+</script>
